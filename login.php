b6baad05155dd67c122f53c0b8cc078e6d36226e
grantbrooks18 <grantbrooks18@gmail.com>
2021-11-07 09:20:44 -0500

    added beggining login functionality

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+
+  $user = 5//mysqli_real_escape_string($_POST['uname']);
+  $pass = 5//mysqli_real_escape_string($_POST['psw']);
+
+  echo $pass;
--- script.js
+++ script.js
@@ -22,24 +22,9 @@ function openPage(pageName, elmnt, color) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
