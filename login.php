92500caff40483d190be3e0ae2ee299bc7cb77fe
grantbrooks18 <grantbrooks18@gmail.com>
2021-11-19 16:25:40 -0500

    php

--- login.php
+++ login.php
@@ -1,12 +1,18 @@
 <?php
-  $link = mysqli_connect(10.0.0.132, "root", "", "demo");
+  $servername = 10.0.0.132;
+  $username = "grant";
+  $password = "fergus1732";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
 
   // Check connection
-  if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
+  echo "Connected successfully";
 
-  $sql = "INSERT INTO Security (username, lpassword VALUES ('Peter', 'Parker'))";
+  $sql = "INSERT INTO Login (username, lpassword VALUES ('Peter', 'Parker'))";
 
   if(mysqli_query($link, $sql)){
       echo "Records inserted successfully.";
