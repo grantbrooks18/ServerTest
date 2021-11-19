681152da45fcaa13ae28f3624d725286a2147982
grantbrooks18 <grantbrooks18@gmail.com>
2021-11-19 12:13:43 -0500

    php

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
-
 <?php
+// define variables and set to empty values
+$uname = $psw = "";
 
-      echo $_POST["uname"];
-      echo "Your email address is:" echo $_POST["psw"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$uname = test_input($_POST["uname"]);
+$psw = test_input($_POST["epsw"]);
+}
 
-      ?>
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+
+echo $uname;
+echo $psw;
+?>
